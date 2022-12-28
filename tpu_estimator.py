e97a7ab991e552fb5b192f20bcae388cdd5d8e78
DMonCityU <108636804+DMonCityU@users.noreply.github.com>
2022-12-28 12:02:13 -0800

    Update tpu_estimator.py
    More contrib issues

--- tpu_estimator.py
+++ tpu_estimator.py
@@ -31,18 +31,18 @@ import six
 from six.moves import queue as Queue  # pylint: disable=redefined-builtin
 from six.moves import xrange  # pylint: disable=redefined-builtin
 
-from tensorflow.contrib.tpu.proto import compilation_result_pb2 as tpu_compilation_result
-from tensorflow.contrib.tpu.python.tpu import tensor_tracer
-from tensorflow.contrib.tpu.python.ops import tpu_ops
-from tensorflow.contrib.tpu.python.tpu import error_handling
-from tensorflow.contrib.tpu.python.tpu import session_support
-from tensorflow.contrib.tpu.python.tpu import tpu
-from tensorflow.contrib.tpu.python.tpu import tpu_config
-from tensorflow.contrib.tpu.python.tpu import tpu_context
-from tensorflow.contrib.tpu.python.tpu import tpu_feed
-from tensorflow.contrib.tpu.python.tpu import training_loop
-from tensorflow.contrib.tpu.python.tpu import util as util_lib
-from tensorflow.contrib.training.python.training import hparam
+from tensorflow.compat.v1.estimator.tpu.proto import compilation_result_pb2 as tpu_compilation_result
+from tensorflow.compat.v1.estimator.tpu.python.tpu import tensor_tracer
+from tensorflow.compat.v1.estimator.tpu.python.ops import tpu_ops
+from tensorflow.compat.v1.estimator.tpu.python.tpu import error_handling
+from tensorflow.compat.v1.estimator.tpu.python.tpu import session_support
+from tensorflow.compat.v1.estimator.tpu.python.tpu import tpu
+from tensorflow.compat.v1.estimator.tpu.python.tpu import tpu_config
+from tensorflow.compat.v1.estimator.tpu.python.tpu import tpu_context
+from tensorflow.compat.v1.estimator.tpu.python.tpu import tpu_feed
+from tensorflow.compat.v1.estimator.tpu.python.tpu import training_loop
+from tensorflow.compat.v1.estimator.tpu.python.tpu import util as util_lib
+from tensorflow.compat.v1.estimator.training.python.training import hparam
 from tensorflow.core.framework import variable_pb2
 from tensorflow.core.framework.summary_pb2 import Summary
 from tensorflow.core.protobuf import config_pb2
