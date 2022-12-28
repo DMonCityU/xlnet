82b7649b3aee849cf877a808461e2e823edc92fe
DMonCityU <108636804+DMonCityU@users.noreply.github.com>
2022-12-28 14:38:17 -0800

    update modeling.py

--- modeling.py
+++ modeling.py
@@ -98,7 +98,9 @@ def post_attention(h, attn_vec, d_model, n_head, d_head, dropout, is_training,
   if residual:
     # Testing without the possible multiple arguments. May need to use keras concatenate for attn_out + h
     # This WILL NOT TRAIN PROPERLY as is.
+    input("What is attn_out?")
     output = tf.keras.layers.LayerNormalization(attn_out, axis = -1)
+    input("It worked!")
     # output = tf.keras.layers.LayerNormalization(attn_out + h, axis = -1)
     #output = tf.compat.v1.layers.layer_norm(attn_out + h, begin_norm_axis=-1,
     #                                      scope='LayerNorm')
